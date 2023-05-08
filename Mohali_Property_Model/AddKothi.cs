419ad6b2f326208b23a3b7cdc41a5c9058ec63d2
ishathedeveloper <ishagupta4033@gmail.com>
2023-05-08 17:24:14 +0530

    Added Api for listing of kothi,and to add kothi

---
+++ Mohali_Property_Model/AddKothi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohali_Property_Model
+{
+    public  class AddKothi
+    {
+        public int kothi_id { get; set; }
+        public int kothi_Number { get; set; }
+        public string dimension { get; set; }
+        public string kothi_unit { get; set;}
+
+        public string block { get; set; }
+        public string kothi_size { get; set; }
+        public float unit_rate { get; set; }
+        public float price { get; set; }
+        public float booking_amount { get; set; }
+        public string status { get; set; }
+        public float token_amount { get; set; }
+
+    }
+}
