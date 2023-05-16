e3c110ecd22376d5274dea6f76b6047eaac2ed1c
sukhwinder <uic.17bca1097@gmail.com>
2023-05-16 12:40:47 +0530

    added the token module and added cards on dashboard

--- Mohali_Property/APICall/Admin/ManageToken/ITokenRepository.cs
+++ Mohali_Property/APICall/Admin/ManageToken/ITokenRepository.cs
@@ -5,5 +5,7 @@ namespace Mohali_Property_Web.APICall.Admin.ManageToken
     public interface ITokenRepository
     {
         public Task<List<TokenVM>> gettokenlists();
+        public Task<int> AddToken(TokenModel token);
+        public Task<int> delete_token(int id);
     }
 }
--- Mohali_Property/APICall/Admin/ManageToken/TokenRepository.cs
+++ Mohali_Property/APICall/Admin/ManageToken/TokenRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Mohali_Property_Model;
 using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 using static Mohali_Property_Web.Extension.Configuration;
 
 namespace Mohali_Property_Web.APICall.Admin.ManageToken
@@ -11,6 +12,24 @@ namespace Mohali_Property_Web.APICall.Admin.ManageToken
         {
             _configuration = configuration;
         }
+
+        public async Task<int> AddToken(TokenModel token)
+        {
+            var url = "/api/TokenApi/add_token";
+            var response = await ApiCall.Initial(_configuration).PostAsJsonAsync(url, token);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+                var usrDetail = JsonConvert.DeserializeObject<int>(stringResponse);
+                return usrDetail;
+            }
+            else
+            {
+                Console.WriteLine("Internal server Error");
+                return 0;
+            }
+        }
+
         public async Task<List<TokenVM>> gettokenlists()
         {
             var url = "/api/TokenApi/gettokenlist";
@@ -26,6 +45,24 @@ namespace Mohali_Property_Web.APICall.Admin.ManageToken
             {
                 Console.WriteLine("Internal server Error");
                 return null;
+            }
+        }
+
+        public async Task<int> delete_token(int id)
+        {
+            var url = "/api/TokenApi/delete_token?id="+id;
+
+            var response = await ApiCall.Initial(_configuration).GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+                var _usrDetail = JsonConvert.DeserializeObject<int>(stringResponse);
+                return _usrDetail;
+            }
+            else
+            {
+                Console.WriteLine("Internal server Error");
+                return 0;
             }
         }
     }
