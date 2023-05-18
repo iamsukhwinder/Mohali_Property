a1d7a32230c52bc80344a85bdfbeab9832872fdb
ishathedeveloper <ishagupta4033@gmail.com>
2023-05-18 14:40:30 +0530

    new configuration added

---
+++ MohaliProperty.Extensions/Extensions/Configurations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohaliProperty.Extensions.Extensions
+{
+    public static class Configurations
+    {
+        private static IConfiguration _configuration;
+
+        public static HttpClient Initial(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var baseurl = _configuration.GetValue<string>("WebAPIBaseUrl");
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(baseurl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+
+    }
+}
