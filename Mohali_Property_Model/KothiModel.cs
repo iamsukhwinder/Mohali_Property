7d4dbd685c743ebda7bdbe9d4ac8e920bf74e431
ishathedeveloper <ishagupta4033@gmail.com>
2023-05-19 13:20:51 +0530

    Server side validations on 4 models

--- Mohali_Property_Model/KothiModel.cs
+++ Mohali_Property_Model/KothiModel.cs
@@ -13,14 +13,18 @@ namespace MohaliProperty.Model
         public int kothi_id { get; set; }
         public int kothi_Number { get; set; }
         public string dimension { get; set; }
-        public string kothi_unit { get; set;}
+  //      public string kothi_unit { get; set;}
         public string block { get; set; }
+        public string kothi_description { get; set; }
         public string kothi_size { get; set; }
-        public double unit_rate { get; set; }
+    //    public double unit_rate { get; set; }
         public double price { get; set; }
+        public double plot_area { get; set; }
+        public int bhk { get; set; }
+
         public double booking_amount { get; set; }
         public string status { get; set; }
-        public double token_amount { get; set; }
+  //      public double token_amount { get; set; }
         public int hold { get; set; }
 
     }
