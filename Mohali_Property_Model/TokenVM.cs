056ec091ad3763211b4cd3161907c8dbf3e45a60
sukhwinder <uic.17bca1097@gmail.com>
2023-05-25 12:12:54 +0530

    make develop runable

--- Mohali_Property_Model/TokenVM.cs
+++ Mohali_Property_Model/TokenVM.cs
@@ -12,7 +12,7 @@ namespace Mohali_Property_Model
         public int kothi_Number { get; set; }
         public double kothi_size { get; set; }  
         public string customer_name { get; set; }
-        public int mobile_number { get; set; }
+        public string mobile_number { get; set; }
         public double price { get; set; }
         public double booking_amount { get; set; }
         //public double recieved_ammount {  get; set; }
