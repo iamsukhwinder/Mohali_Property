b3d58d8c1f2dc1bdcd9ed8f93031441050fe3a56
sukhwinder <uic.17bca1097@gmail.com>
2023-05-31 15:08:31 +0530

    added booking api and repository to get booking detail data

---
+++ Mohali_Property/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MohaliProperty.Services.WebServices.Admin.ManageKothi;
+using MohaliProperty.Services.WebServices.Booking;
+
+namespace MohaliProperty.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IManageKothi _kothi;
+        private readonly IBookingRepository _booking;
+        public BookingController(IManageKothi kothi,IBookingRepository booking)
+        {
+            _kothi = kothi;
+            _booking = booking;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+
+            var booling_detail =await _booking.getbookingdetail(id);
+            //if(User.Identity.IsAuthenticated == true && User.IsInRole("User"))
+            //{
+
+            
+            return View();
+
+            //}
+            //else
+            //{
+            //    return RedirectToAction("SignUp", "Home");
+            //}
+        }
+    }
+}
