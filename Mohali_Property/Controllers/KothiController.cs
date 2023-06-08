3361813e9a1320e27d1ca977ff6d4b4e3c94c3ac
Bishesh123 <bdhiman2514.ca20@chitkara.edu.in>
2023-06-08 11:02:25 +0530

    Changes in Kothi Module

--- Mohali_Property/Controllers/KothiController.cs
+++ Mohali_Property/Controllers/KothiController.cs
@@ -43,57 +43,68 @@ namespace Mohali_Property_Web.Controllers
         public async Task<IActionResult> Add_Kothi(IFormCollection kothi_image,KothiModel kothidata)
 
         {
-
-            if (kothi_image.Files.Count >= 2)
+            if (ModelState.IsValid)
             {
-                ViewData["multiple_not_valid"] = "* multiple images are not allowed";
-                return View();
-            }
-            if (kothi_image.Files[0].ContentType != "image/jpeg" && kothi_image.Files[0].ContentType != "image/png" && kothi_image.Files[0].ContentType != "image/jpg")
-            {
-                ViewData["image_type"] = "* image type is invalid upload only jpeg,png,jpg";
-                return View();
-
-            }
-
-         
-            if (kothi_image.Files.Count != 0)
-            {
-                var file = kothi_image.Files[0];
-                var size = file.Length;
-                string kothi_image_name = file.FileName;
-
-                var webPath = _hostingEnvironment.WebRootPath;
-                var filePath = Path.Combine(webPath, "Image/kothi_images");
-                filePath = Path.Combine(filePath, kothidata.kothi_Number + file.FileName);
-                KothiModel kothi = new KothiModel();
-                kothi.kothi_Number = kothidata.kothi_Number;
-                kothi.block = kothidata.block;
-                kothi.kothi_size = kothidata.kothi_size;
-                kothi.kothi_description = kothidata.kothi_description;
-                kothi.dimension = kothidata.dimension;
-                kothi.plot_area = kothidata.plot_area;
-                kothi.price = kothidata.price;
-                kothi.bhk = kothidata.bhk;
-                kothi.booking_amount = kothidata.booking_amount;
-                kothi.status = kothidata.status;
-                kothi.kothi_image = kothidata.kothi_Number + file.FileName;
-                kothi.hold = 1;
-
-                var result = await _kothi.Add_Kothi(kothi);
-                using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                if (kothi_image.Files.Count >= 2)
                 {
-                    await file.CopyToAsync(stream);
+                    ViewData["multiple_not_valid"] = "* multiple images are not allowed";
+                    return View();
                 }
+                if (kothi_image.Files[0].ContentType != "image/jpeg" && kothi_image.Files[0].ContentType != "image/png" && kothi_image.Files[0].ContentType != "image/jpg")
+                {
+                    ViewData["image_type"] = "* image type is invalid upload only jpeg,png,jpg";
                     return View();
 
                 }
-              
-            
 
+
+                if (kothi_image.Files.Count != 0)
+                {
+                    var file = kothi_image.Files[0];
+                    var size = file.Length;
+                    string kothi_image_name = file.FileName;
+
+                    var webPath = _hostingEnvironment.WebRootPath;
+                    var filePath = Path.Combine(webPath, "Image/kothi_images");
+                    filePath = Path.Combine(filePath, kothidata.kothi_Number + file.FileName);
+                    KothiModel kothi = new KothiModel();
+                    kothi.kothi_Number = kothidata.kothi_Number;
+                    kothi.block = kothidata.block;
+                    kothi.kothi_size = kothidata.kothi_size;
+                    kothi.kothi_description = kothidata.kothi_description;
+                    kothi.dimension = kothidata.dimension;
+                    kothi.plot_area = kothidata.plot_area;
+                    kothi.price = kothidata.price;
+                    kothi.bhk = kothidata.bhk;
+                    kothi.booking_amount = kothidata.booking_amount;
+                    kothi.status = kothidata.status;
+                    kothi.kothi_image = kothidata.kothi_Number + file.FileName;
+                    kothi.hold = 1;
+                    kothi.company_id = kothidata.company_id;
+
+                    var result = await _kothi.Add_Kothi(kothi);
+                    using (var stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    return View();
+
+                }
+
+                return View();
+            }
             else
             {
-
+                var compnies = await _comp.GetComopanyList();
+                List<SelectListItem> compddl = new List<SelectListItem>();
+                foreach (Company_profileVM company in compnies)
+                {
+                    SelectListItem compselect = new SelectListItem();
+                    compselect.Text = company.company_name;
+                    compselect.Value = company.company_id.ToString();
+                    compddl.Add(compselect);
+                }
+                ViewData["compddl"] = compddl;
                 return View();
 
 
@@ -116,7 +127,7 @@ namespace Mohali_Property_Web.Controllers
         public async Task<IActionResult> Edit_kothi(int id)
         {
             var kothies = await _kothi.Edit_kothi(id);
-            return View(kothies);
+            return PartialView(kothies);
           
 
         }
