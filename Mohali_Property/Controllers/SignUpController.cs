a1578dfcedbe89bd1d73615f93bc86b0598add30
Bishesh123 <bdhiman2514.ca20@chitkara.edu.in>
2023-06-13 09:42:05 +0530

    Changes in Sign Up Same Email should not be registered

--- Mohali_Property/Controllers/SignUpController.cs
+++ Mohali_Property/Controllers/SignUpController.cs
@@ -34,60 +34,65 @@ namespace MohaliProperty.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                DateTime created_date = DateTime.Now.Date;
+                DateTime created_date = DateTime.Now.Date;  
                 obj.created_date = created_date;
                 var data = await _manageCustomer.AddCustomer(obj);
-
-
-                var file = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Email_Image", "Registrationpage.html");
-                String SendMailFrom = "bisheshdhiman5514@gmail.com";
-                String SendMailTo = obj.customer_email;
-                String SendMailSubject = "Registration Successfully";
-                //String SendMailBody = "<a href='http://localhost:5063/Home/Login'>Thanks for registration for us  [Please click here to login] </a>";
-                String SendMailBody = System.IO.File.ReadAllText(file);
-                try
+                if (data == 3)
                 {
-                    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587);
-                    SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    MailMessage email = new MailMessage();
-                    // START
-                    email.From = new MailAddress(SendMailFrom);
-                    email.To.Add(SendMailTo);
-                    email.CC.Add(SendMailFrom);
-                    email.Subject = SendMailSubject;
-                    string i = ("<img src=https://media.publit.io/file/q_80/chrmpWebsite/thank-you-form.png>" + "<br><br><br>");
-                    string v = ("Thanks for Registration Your username is -->" + obj.customer_email + " and your password is--> " + obj.mobile_number) + "<br>" + ("<a href='http://localhost:5130/Home/Login'>Thanks for registration for us  [Please click here to login] </a>");
-                    email.Body = i + v;
-                    email.IsBodyHtml = true;
-                    //END
-                    SmtpServer.Timeout = 10000;
-                    SmtpServer.EnableSsl = true;
-                    SmtpServer.UseDefaultCredentials = false;
-                    SmtpServer.Credentials = new NetworkCredential(SendMailFrom, "neenaeaznlfjlivo");
-                    SmtpServer.Send(email);
-
-
-
-
-                }
-                catch (Exception ex)
-                {
-
-                }
-                if (data != null)
-                {
-                    TempData["success_msg"] = "Succesfull SignUp";
+                    TempData["msg"] = "Email is already registered";
                     return RedirectToAction("SignUp", "SignUp");
                 }
-            }
-            //else
-            //{
+                else
+                {
 
-            //    // return View("SignUp", "Home");
-            //    return RedirectToAction("SignUp", "SignUp");
-            //}
-            //   return View("SignUp", "Home");
+                       
+                    var file = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Email_Image", "Registrationpage.html");
+                    String SendMailFrom = "bisheshdhiman5514@gmail.com";
+                    String SendMailTo = obj.customer_email;
+                    String SendMailSubject = "Registration Successfully";
+                    //String SendMailBody = "<a href='http://localhost:5063/Home/Login'>Thanks for registration for us  [Please click here to login] </a>";
+                    String SendMailBody = System.IO.File.ReadAllText(file);
+                    try
+                    {
+                        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587);
+                        SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network; 
+                        MailMessage email = new MailMessage();
+                        // START
+                        email.From = new MailAddress(SendMailFrom);
+                        email.To.Add(SendMailTo);
+                        email.CC.Add(SendMailFrom);
+                        email.Subject = SendMailSubject;
+                        string i = ("<img src=https://media.publit.io/file/q_80/chrmpWebsite/thank-you-form.png>" + "<br><br><br>");
+                        string v = ("Thanks for Registration Your username is -->" + obj.customer_email + " and your password is--> " + obj.mobile_number) + "<br>" + ("<a href='http://localhost:5130/Home/Login'>Thanks for registration for us  [Please click here to login] </a>");
+                        email.Body = i + v;
+                        email.IsBodyHtml = true;
+                        //END
+                        SmtpServer.Timeout = 10000;
+                        SmtpServer.EnableSsl = true;
+                        SmtpServer.UseDefaultCredentials = false;
+                        SmtpServer.Credentials = new NetworkCredential(SendMailFrom, "neenaeaznlfjlivo");
+                        SmtpServer.Send(email);
+
+                        TempData["success_msg"] = "Succesfull SignUp--> Please Check your Email for Username and Password";
+                        return RedirectToAction("SignUp", "SignUp");
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        return View();
+                    }
+                }   
+               
+            }
+            else
+            {
+
             return View();
+                //    // return View("SignUp", "Home");
+                //    return RedirectToAction("SignUp", "SignUp");
+            }
+            //   return View("SignUp", "Home");
 
         }
 
--- Mohali_Property_API/Controllers/CustomerController.cs
+++ Mohali_Property_API/Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Mohali_Property_Model;
 using MohaliProperty.Dbcontext.Models;
 using MohaliProperty.Model;
+using System.Web.Helpers;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MohaliProperty.API.Controllers
@@ -41,7 +42,16 @@ namespace MohaliProperty.API.Controllers
         [HttpPost("add_customer")]
         public int add_customer(CustomerModel obj)
         {
-            List<SqlParameter> parms = new List<SqlParameter>
+
+            var result = _context.Logins.Where(m => m.username == obj.customer_email).FirstOrDefault();
+            if (result != null)
+            {
+                return 3;
+            }
+            else
+            {
+
+                List<SqlParameter> parms = new List<SqlParameter>
             {
                   //new SqlParameter { ParameterName = "@customer_id", Value = obj.customer_id },
                   new SqlParameter { ParameterName = "@customer_name", Value = obj.customer_name },
@@ -51,21 +61,22 @@ namespace MohaliProperty.API.Controllers
                   new SqlParameter { ParameterName = "@customer_email", Value = obj.customer_email },
                   new SqlParameter { ParameterName = "@mobile_number", Value = obj.mobile_number },
                   new SqlParameter { ParameterName = "@created_date", Value = obj.created_date},
-                  
+
 
 
             };
 
-            int n = _context.Database.ExecuteSqlRaw("EXEC add_customer @customer_name,@parent_name,@address,@city,@customer_email,@mobile_number,@created_date", parms.ToArray());
+                int n = _context.Database.ExecuteSqlRaw("EXEC add_customer @customer_name,@parent_name,@address,@city,@customer_email,@mobile_number,@created_date", parms.ToArray());
 
-            if (n == 0)
-            {
-                return 0;
-            }
-            else
-            {
-                return 1;
+                if (n == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
 
+                }
             }
 
 
