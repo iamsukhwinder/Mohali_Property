1aef974514a78c98223db2109cf1f8a3172f0134
sukhwinder <uic.17bca1097@gmail.com>
2023-06-13 17:55:28 +0530

    Search kothi is now working and add active link functionallity in navbar

--- Mohali_Property/wwwroot/js/Login/LoginValidations.js
+++ Mohali_Property/wwwroot/js/Login/LoginValidations.js
@@ -1,46 +1,48 @@
-﻿<script>
+﻿$(document).ready(function () {
+    debugger;
+    $("#SignIn").addClass("active");
+});
 
 
 
-    function validate() {
-        var result = true;
+    //function validate() {
+    //    var result = true;
 
-    var username = $("#username").val();
+    //var username = $("#username").val();
 
-    if (username == "") {
-        result = false;
-    $("#eusername").html("please enter username");
+    //if (username == "") {
+    //    result = false;
+    //$("#eusername").html("please enter username");
 
-        }
-    else {
-        $("#eusername").html("");
-        }
-    var password = $("#password").val();
-    if (password == "") {
-        result = false;
-    $("#epassword").html("please enter password");
+    //    }
+    //else {
+    //    $("#eusername").html("");
+    //    }
+    //var password = $("#password").val();
+    //if (password == "") {
+    //    result = false;
+    //$("#epassword").html("please enter password");
 
-        }
-    else {
-        $("#epassword").html("");
-        }
-    if(result == false){
-        $("#einvalid_input").hide();
-        }
+    //    }
+    //else {
+    //    $("#epassword").html("");
+    //    }
+    //if(result == false){
+    //    $("#einvalid_input").hide();
+    //    }
 
-    return result;
-    }
-
-
-
-
-
-
-
+    //return result;
+    //}
 
 
 
 
 
 
-</script>
+
+
+
+
+
+
+
